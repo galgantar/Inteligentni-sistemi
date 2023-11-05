98e42a30391962990e5db4a364a375f8830e728c
DKastelic <domenkast@gmail.com>
2023-11-05 16:24:31 +0100

    Comment on dataset parsing.

--- seminarska 1/dataset_explorer.py
+++ seminarska 1/dataset_explorer.py
@@ -8,6 +8,9 @@ with open("dataset.csv") as file:
     for row in reader:
         print(', '.join(row))
 
+        # row[1] is x, row[2] is y
+        # take substring to remove brackets
+        # split by comma and space
         xs = np.fromstring(row[1][1:-1], sep=", ")
         ys = np.fromstring(row[2][1:-1], sep=", ")
 
